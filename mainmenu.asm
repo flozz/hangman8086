01a61452c57152f28863bbf1230f6265726d06ff
Fabien LOISON <flo@flogisoft.com>
2011-05-20 11:03:00 +0200

    * Arrows can now be used in the menu

--- mainmenu.asm
+++ mainmenu.asm
@@ -58,33 +58,41 @@ call _draw_ui
 mov HELP_STR, offset main_menu_help
 call _print_help
 
+;Draw the menu
+call _draw_main_menu
+
 jmp main_menu_st
 
 ;Play a sound when the item change
 main_menu_st_snd:
     mov SOUND, offset SND_MENU_CH_ITEM
+    call _draw_main_menu
     call _play_sound
 
 ;The main menu
 main_menu_st:
-    ;Draw the menu
-    call _draw_main_menu
-
     ;Wait for input
-    mov ah, 0x07
-    int 0x21
+    main_menu_input:
+    mov ax, 0x0000
+    int 0x16
 
     ;Test the input
     cmp al, ' '         ;Space
-    je main_menu_kb_space
+    je main_menu_validate
     cmp al, 0x0D        ;Enter
-    je main_menu_kb_enter
+    je main_menu_validate
+    cmp ah, 0x50        ;Down arrow
+    je main_menu_movedown
+    cmp ah, 0x48        ;Up arrow
+    je main_menu_moveup
+    cmp ax, 0x011B      ;Escape
+    je main_menu_end
 
     ;Not a valid input... try again ! :p
     jmp main_menu_st
 
-    ;Space key pressed
-    main_menu_kb_space:
+    ;Move down in the menu
+    main_menu_movedown:
         inc main_menu_selected
 
         cmp main_menu_selected, main_menu_items_numb
@@ -94,8 +102,20 @@ main_menu_st:
 
         jmp main_menu_st_snd
 
-    ;Enter key pressed
-    main_menu_kb_enter:
+    ;Move up in the menu
+    main_menu_moveup:
+        dec main_menu_selected
+
+        cmp main_menu_selected, -1
+        jne main_menu_st_snd
+
+        mov main_menu_selected, main_menu_items_numb
+        dec main_menu_selected
+
+        jmp main_menu_st_snd
+
+    ;Validate the selected item
+    main_menu_validate:
         mov SOUND, offset SND_MENU_VALID
         call _play_sound
 
@@ -169,7 +189,9 @@ main_menu_selected db 1  ;The selected item of the menu
 
 
 ;=========================== Datas for _main_menu() and _draw_main_menu() ====
-main_menu_help  db "<Space> Select the next item   <Enter> Validate the item$"
+main_menu_help  db 0xDA,0x18,0x19,0xBF," Navigate   ",0xDA,"Enter",0xBF
+                db " Validate                                    ",0xDA
+                db "Esc",0xBF, " Quit$"
 main_menu_items db "  Single Player$"
                 db "  Two players  $"
                 db "  Options      $"
